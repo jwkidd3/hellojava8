a22405e64c659c81753fb6c2987fe88da54c1f89
jwkidd3 <jkidd@kiddcorp.com>
2017-01-16 06:53:49 -0800

    new

--- Patt.java
+++
@@ -1,9 +0,0 @@
-import java.util.regex.Pattern;
-
-public class Patt {
-    public static void main(String...args) {
-        Pattern patt = Pattern.compile("[, ]+");
-        patt.splitAsStream("a, b, c")
-            .forEach(System.out::println);
-    }
-}
